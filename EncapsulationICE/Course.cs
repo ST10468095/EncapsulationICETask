13de8ef8b4819d5885dd3b1a96552f80541ae55a
Nathan Beaumont <ST10468095@myemeris.edu.za>
2026-04-20 19:14:34 +0200

    Add project files.

---
+++ EncapsulationICE/Course.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationICE
+{
+    internal class Course
+    {
+        private int grade;
+
+
+        public int Grade
+        {
+            get { return grade; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    grade = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid grade. must be between 0 and 100.");
+                }
+            }
+        }
+
+
+        public bool IsPass
+        {
+            get { return grade >= 50; }
+        }
+    }
+}
---
+++ EncapsulationICE/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationICE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course = new Course();
+
+            course.Grade = 80;
+
+            Console.WriteLine("Grade: " + course.Grade);
+            Console.WriteLine("Passed: " + course.IsPass);
+        }
+    }
+}
